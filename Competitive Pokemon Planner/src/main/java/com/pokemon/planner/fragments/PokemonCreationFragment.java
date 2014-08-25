c53ca4cb2531cf77afe615382001699a9ac46206
llucic <luka.lucic@gmail.com>
2014-08-25 16:36:14 -0400

    stat calculators

--- Competitive Pokemon Planner/src/main/java/com/pokemon/planner/fragments/PokemonCreationFragment.java
+++ Competitive Pokemon Planner/src/main/java/com/pokemon/planner/fragments/PokemonCreationFragment.java
@@ -12,15 +12,14 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.pokemon.planner.R;
 import com.pokemon.planner.dataobjects.PokemonBase;
 import com.pokemon.planner.filters.InputFilterMinMax;
+import com.pokemon.planner.util.Calculator;
 import com.pokemon.planner.util.DataStore;
 import com.pokemon.planner.util.Nature;
-import com.pokemon.planner.util.QuickAutoCompleteTextView;
-
-import java.util.ArrayList;
 
 /**
  * Created by Luka on 6/27/2014.
@@ -39,6 +38,13 @@ public class PokemonCreationFragment extends Fragment {
     EditText spaEvValue;
     EditText spdEvValue;
     EditText speEvValue;
+
+    TextView hpTotalValue;
+    TextView atkTotalValue;
+    TextView defTotalValue;
+    TextView spaTotalValue;
+    TextView spdTotalValue;
+    TextView speTotalValue;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -88,32 +94,204 @@ public class PokemonCreationFragment extends Fragment {
 
         natureChooser.setAdapter(adapter);
 
+        hpTotalValue = (TextView)v.findViewById(R.id.lbl_total_hp);
+        hpTotalValue.setText("" + Calculator.getHpForLevel(100,
+                (int)chosenPokemonBase.getBaseStats().hp,
+                0,
+                31));
+        atkTotalValue = (TextView)v.findViewById(R.id.lbl_total_atk);
+        atkTotalValue.setText("" + Calculator.getStatForLevel(100,
+                (int)chosenPokemonBase.getBaseStats().attack,
+                0,
+                31,
+                getNatureModifier(getResources().getStringArray(R.array.stats)[1])));
+        defTotalValue = (TextView)v.findViewById(R.id.lbl_total_def);
+        defTotalValue.setText("" + Calculator.getStatForLevel(100,
+                (int)chosenPokemonBase.getBaseStats().defence,
+                0,
+                31,
+                getNatureModifier(getResources().getStringArray(R.array.stats)[2])));
+        spaTotalValue = (TextView)v.findViewById(R.id.lbl_total_spa);
+        spaTotalValue.setText("" + Calculator.getStatForLevel(100,
+                (int)chosenPokemonBase.getBaseStats().sp_attack,
+                0,
+                31,
+                getNatureModifier(getResources().getStringArray(R.array.stats)[3])));
+        spdTotalValue = (TextView)v.findViewById(R.id.lbl_total_spd);
+        spdTotalValue.setText("" + Calculator.getStatForLevel(100,
+                (int)chosenPokemonBase.getBaseStats().sp_defence,
+                0,
+                31,
+                getNatureModifier(getResources().getStringArray(R.array.stats)[4])));
+        speTotalValue = (TextView)v.findViewById(R.id.lbl_total_spe);
+        speTotalValue.setText("" + Calculator.getStatForLevel(100,
+                (int)chosenPokemonBase.getBaseStats().speed,
+                0,
+                31,
+                getNatureModifier(getResources().getStringArray(R.array.stats)[5])));
+
         levelChooser = (EditText)v.findViewById(R.id.pokemon_level);
         levelChooser.setFilters(new InputFilter[] {new InputFilterMinMax(1,100)});
+        levelChooser.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int level = Integer.parseInt(s.toString());
+                double hp = Calculator.getHpForLevel(level, (int) chosenPokemonBase.getBaseStats().hp, Integer.parseInt(hpEvValue.getText().toString()), 31);
+                hpTotalValue.setText("" + hp);
+                double atk = Calculator.getStatForLevel(level, (int) chosenPokemonBase.getBaseStats().attack, Integer.parseInt(atkEvValue.getText().toString()), 31, getNatureModifier("Attack"));
+                atkTotalValue.setText("" + atk);
+                double def = Calculator.getStatForLevel(level, (int) chosenPokemonBase.getBaseStats().defence, Integer.parseInt(defEvValue.getText().toString()), 31, getNatureModifier("Attack"));
+                defTotalValue.setText("" + def);
+                double spa = Calculator.getStatForLevel(level, (int) chosenPokemonBase.getBaseStats().sp_attack, Integer.parseInt(spaEvValue.getText().toString()), 31, getNatureModifier("Attack"));
+                spaTotalValue.setText("" + spa);
+                double spd = Calculator.getStatForLevel(level, (int) chosenPokemonBase.getBaseStats().sp_defence, Integer.parseInt(spdEvValue.getText().toString()), 31, getNatureModifier("Attack"));
+                spdTotalValue.setText("" + spd);
+                double spe = Calculator.getStatForLevel(level,(int)chosenPokemonBase.getBaseStats().speed, Integer.parseInt(speEvValue.getText().toString()), 31, getNatureModifier("Attack"));
+                speTotalValue.setText("" + spe);
+            }
+        });
         levelChooser.setText("1");
 
         hpEvValue = (EditText)v.findViewById(R.id.txt_hp_ev);
         hpEvValue.setFilters(new InputFilter[] {new InputFilterMinMax(0,252)});
+        hpEvValue.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int level = Integer.parseInt(levelChooser.getText().toString());
+                double hp = Calculator.getHpForLevel(level,(int)chosenPokemonBase.getBaseStats().hp, Integer.parseInt(s.toString()), 31);
+                hpTotalValue.setText("" + hp);
+            }
+        });
         hpEvValue.setText("0");
 
         atkEvValue = (EditText)v.findViewById(R.id.txt_atk_ev);
         atkEvValue.setFilters(new InputFilter[] {new InputFilterMinMax(0,252)});
+        atkEvValue.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int level = Integer.parseInt(levelChooser.getText().toString());
+                double atk = Calculator.getStatForLevel(level,(int)chosenPokemonBase.getBaseStats().attack, Integer.parseInt(s.toString()), 31, 1);
+                atkTotalValue.setText("" + atk);
+            }
+        });
         atkEvValue.setText("0");
 
         defEvValue = (EditText)v.findViewById(R.id.txt_def_ev);
         defEvValue.setFilters(new InputFilter[] {new InputFilterMinMax(0,252)});
+        defEvValue.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int level = Integer.parseInt(levelChooser.getText().toString());
+                double def = Calculator.getStatForLevel(level,(int)chosenPokemonBase.getBaseStats().defence, Integer.parseInt(s.toString()), 31, 1);
+                defTotalValue.setText("" + def);
+            }
+        });
         defEvValue.setText("0");
 
         spaEvValue = (EditText)v.findViewById(R.id.txt_spa_ev);
         spaEvValue.setFilters(new InputFilter[] {new InputFilterMinMax(0,252)});
+        spaEvValue.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int level = Integer.parseInt(levelChooser.getText().toString());
+                double spAtk = Calculator.getStatForLevel(level,(int)chosenPokemonBase.getBaseStats().sp_attack, Integer.parseInt(s.toString()), 31, 1);
+                spaTotalValue.setText("" + spAtk);
+            }
+        });
         spaEvValue.setText("0");
 
         spdEvValue = (EditText)v.findViewById(R.id.txt_spd_ev);
         spdEvValue.setFilters(new InputFilter[] {new InputFilterMinMax(0,252)});
+        spdEvValue.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int level = Integer.parseInt(levelChooser.getText().toString());
+                double spDef = Calculator.getStatForLevel(level,(int)chosenPokemonBase.getBaseStats().sp_defence, Integer.parseInt(s.toString()), 31, 1);
+                spdTotalValue.setText("" + spDef);
+            }
+        });
         spdEvValue.setText("0");
 
         speEvValue = (EditText)v.findViewById(R.id.txt_spe_ev);
         speEvValue.setFilters(new InputFilter[] {new InputFilterMinMax(0,252)});
+        speEvValue.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int level = Integer.parseInt(levelChooser.getText().toString());
+                double speed = Calculator.getStatForLevel(level,(int)chosenPokemonBase.getBaseStats().speed, Integer.parseInt(s.toString()), 31, 1);
+                speTotalValue.setText("" + speed);
+            }
+        });
         speEvValue.setText("0");
 
         return v;
@@ -147,5 +325,20 @@ public class PokemonCreationFragment extends Fragment {
             abilities[pokemon.getAbilities().length] = pokemon.getHiddenAbility();
         }
         return abilities;
+    }
+
+    private double getNatureModifier(String stat) {
+        for(Nature nature : DataStore.natures) {
+            if (natureChooser.getText().toString().equalsIgnoreCase(nature.toString())) {
+                if (nature.boosted().equalsIgnoreCase(stat)) {
+                    return 1.1;
+                }
+                if (nature.hindered().equalsIgnoreCase(stat)) {
+                    return 0.9;
+                }
+                return 1;
+            }
+        }
+        return 1;
     }
 }
